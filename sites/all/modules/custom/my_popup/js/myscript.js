7217c4412bd28a5767ae26cbd688281c83cab1b8
Mahesh Patil <mahesh.patil@iksula.com>
2015-08-12 11:39:26 +0530

    new pop up functionality

---
+++ sites/all/modules/custom/my_popup/js/myscript.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function () {
+    window.setInterval(closesplash, 5000); // Call function after 15 seconds.
+    function closesplash() {
+        jQuery.colorbox.close();
+    }
+});
